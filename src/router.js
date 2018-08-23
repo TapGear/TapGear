f064a1c1fa7d477903febcbe5e2770ded052a26e
Joan Lamrack <joanlamrack@gmail.com>
2018-08-23 18:03:17 +0700

    adding firebase

--- src/router.js
+++ src/router.js
@@ -1,6 +1,5 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import LogicTap from './components/LogicTap'
 import Home from './views/Home.vue'
 
 Vue.use(Router)
@@ -21,10 +20,6 @@ export default new Router({
       // this generates a separate chunk (about.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
       component: () => import(/* webpackChunkName: "about" */ './views/About.vue')
-    }, {
-      path: '/game',
-      name: 'game',
-      component: LogicTap
     }
   ]
 })
--- src/store.js
+++ src/store.js
@@ -1,6 +1,6 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
-
+import { db } from '@/firebase.js'
 Vue.use(Vuex)
 
 export default new Vuex.Store({
