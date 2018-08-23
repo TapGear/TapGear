016a23dc05c18e118de4d8914bfd8f6b75aa40d7
Joan Lamrack <joanlamrack@gmail.com>
2018-08-23 17:58:36 +0700

    Adding firebase config

---
+++ src/firebase.js
@@ -0,0 +1,18 @@
+import {initializeApp}from 'firebase'
+
+
+
+const firebaseApp = initializeApp ({
+    apiKey: "AIzaSyBEm7c6vcKGMz55CwWMcC44q60IVkzqcwU",
+    authDomain: "tapgear-ea17e.firebaseapp.com",
+    databaseURL: "https://tapgear-ea17e.firebaseio.com",
+    projectId: "tapgear-ea17e",
+    storageBucket: "tapgear-ea17e.appspot.com",
+    messagingSenderId: "842846043379"
+  });
+
+
+
+  // Get a reference to the database service
+
+  export const db = firebaseApp.database()
