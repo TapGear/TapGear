d49657cda59994ba86d67c4bce9d31500221f1d1
Grayfield <grayfieldmm@outlook.com>
2018-08-24 13:52:17 +0700

    set .env.local

--- src/firebase.js
+++ src/firebase.js
@@ -1,12 +1,23 @@
-import {initializeApp} from 'firebase'
+import { initializeApp } from 'firebase'
+
+console.log(process.env.VUE_APP_FIREBASE_DB_URL)
 
 const firebaseApp = initializeApp({
+<<<<<<< HEAD
   apiKey: process.env.apiKey,
   authDomain: process.env.authDomain,
   databaseURL: process.env.databaseURL,
   projectId: process.env.projectId,
   storageBucket: process.env.storageBucket,
   messagingSenderId: process.env.messagingSenderId
+=======
+  apiKey: process.env.VUE_APP_FIREBASE_API_KEY,
+  authDomain: process.env.VUE_APP_FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.VUE_APP_FIREBASE_DB_URL,
+  projectId: process.env.VUE_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.VUE_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.VUE_APP_FIREBASE_SENDER_ID
+>>>>>>> set .env.local
 })
 
 // Get a reference to the database service
